7c4d4c76c9d9414c62f12f974defc79605cbaa4f
Anesu Gavhera <agavhera@gmail.com>
2021-09-21 23:59:16 -0400

    Recopy eveything

---
+++ public/js/scripts.js
@@ -0,0 +1,17 @@
+$(functcon() {
+    $("#contactusButton").click(function(){
+        $('#contactusModal').modal('show');
+    });
+
+});
+
+$(function() {
+    $("#contactusButton2").click(function(){
+        $('#contactusModal').modal('show');
+    });
+
+});
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
